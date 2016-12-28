84058b964bc225568cf87d397acb6ea931c061d8
IfOu <didiflorian@gmail.com>
2016-12-28 19:34:23 +0100

    + Clean Setup + Add Class MouseMoveEvent

---
+++ src/main/java/MouseMoveEvent.java
@@ -0,0 +1,32 @@
+import java.awt.Point;
+import java.util.EventObject;
+
+/**
+ * The Class MouseMouveEvent
+ */
+public class MouseMoveEvent extends EventObject{
+
+	/** The point of event */
+	private Point point;
+	
+	/**
+	 * Instantiates a new mouse move event.
+	 *
+	 * @param arg0 it's the point of event
+	 */
+	public MouseMoveEvent(Point arg0) {
+		super(arg0);
+		this.point=arg0;
+
+	}
+
+	/**
+	 * Gets the point of the mouse move event.
+	 *
+	 * @return the point
+	 */
+	public Point getPoint() {
+		return this.point;
+	}
+	
+}
