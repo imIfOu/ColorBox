613b505e6e55a2089a73bd5af5c07d49600586ab
IfOu <didiflorian@gmail.com>
2017-01-02 23:09:30 +0100

    -Fix project

--- src/main/java/org/ifou/colorbox/ColorBox.java
+++ src/main/java/org/ifou/colorbox/ColorBox.java
@@ -1,30 +1,5 @@
 package org.ifou.colorbox;
 
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 import java.awt.AWTException;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -88,7 +63,6 @@ import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
-
 
 public class ColorBox extends HBox {
 
@@ -716,7 +690,8 @@ public class ColorBox extends HBox {
 		@Override
 		public void mouseMoved(MouseMoveEvent event) {
 			final Point point = event.getPoint();
-			BufferedImage im = robot.createScreenCapture(new Rectangle((int) point.getX() - 4, (int) point.getY() - 4, 9, 9));
+			BufferedImage im = robot
+					.createScreenCapture(new Rectangle((int) point.getX() - 4, (int) point.getY() - 4, 9, 9));
 			view.setImage(SwingFXUtils.toFXImage(scale(im, 10), null));
 			Platform.runLater(new Runnable() {
 				@Override
